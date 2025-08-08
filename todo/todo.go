64ad7718c5bb84dd7aea994c82877205f60b8f15
Rq4n <ryanlimabatista2@gmail.com>
2025-08-08 06:33:12 -0300

    feat: storage.go, new func newStorage and saveFiles, to save data in json

--- todo/todo.go
+++ todo/todo.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+
 	"github.com/aquasecurity/table"
 )
 
@@ -17,18 +18,34 @@ type Todos []Todo
 
 func (todos *Todos) Add(name string) {
 	task := Todo{
-		Title:      name,
+		Title:     name,
 		Completed: false,
 	}
 	*todos = append(*todos, task)
 }
 
-func (todos *Todos) validateIndex(index int) error {
-	if index < 0 || index >= len(*todos) {
-		err := errors.New("Invalid index")
-		fmt.Println(err)
+func (todos *Todos) Print() {
+	table := table.New(os.Stdout)
+	table.SetRowLines(false)
+	table.SetHeaders("#", "Title", "Completed")
+	for index, t := range *todos {
+		completed := "❌"
+
+		if t.Completed {
+			completed = "✅"
+		}
+		table.AddRow(strconv.Itoa(index), t.Title, completed)
+	}
+	table.Render()
+}
+
+func (todos *Todos) Complete(index int) error {
+	t := *todos
+
+	if err := t.validateIndex(index); err != nil {
 		return err
 	}
+	t[index].Completed = !t[index].Completed
 	return nil
 }
 
@@ -38,34 +55,15 @@ func (todos *Todos) Delete(index int) error {
 	if err := t.validateIndex(index); err != nil {
 		return err
 	}
-
 	*todos = append(t[:index], t[index+1:]...)
-
 	return nil
 }
 
-func (todos *Todos) Print(){
-	table := table.New(os.Stdout)
-	table.SetRowLines(false)
-	table.SetHeaders("#", "Title", "Completed")
-	for index, t := range *todos{
-		completed := "❌"
-
-		if t.Completed {
-			completed = "✅"
-		}
-		table.AddRow(strconv.Itoa(index), t.Title, completed)
-	} 
-	table.Render()
-}
-
-func (todos *Todos) Complete(index int) error {
-	 t := *todos
-
-	if err := t.validateIndex(index); err != nil{
+func (todos *Todos) validateIndex(index int) error {
+	if index < 0 || index >= len(*todos) {
+		err := errors.New("invalid index")
+		fmt.Println(err)
 		return err
 	}
-	t[index].Completed = !t[index].Completed
 	return nil
-	}
-
+}
