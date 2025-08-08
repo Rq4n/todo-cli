ad9c7e485f3d514587139f959b33d4b6246b3f9d
Rq4n <ryanlimabatista2@gmail.com>
2025-08-08 06:34:08 -0300

    fix: storage.go

---
+++ storage/storage.go
@@ -0,0 +1,22 @@
+package storage
+
+import (
+	"encoding/json"
+	"os"
+)
+
+type Storage[T any] struct {
+	FileName string
+}
+ func newStorage[T any] (fileName string) *Storage [T] {
+	return &Storage[T]{FileName: fileName}
+}
+
+func (s *Storage[T]) Save(data T) error{
+	fileData, err := json.MarshalIndent(data, "", "")
+	if err != nil{
+		return err 
+	}
+	return os.WriteFile(s.FileName, fileData, 0644)
+
+}
