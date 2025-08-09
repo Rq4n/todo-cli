8fab742ce7fe8e95f2d3f1d3243267116d81578a
Rq4n <ryanlimabatista2@gmail.com>
2025-08-09 12:16:02 -0300

    feat: saved data in json on main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,16 +1,29 @@
 package main
 
 import (
-	  "github.com/Rq4n/todo-cli/todo"
+	"fmt"
+	"log"
+
+	"github.com/Rq4n/todo-cli/storage"
+	"github.com/Rq4n/todo-cli/todo"
 )
 
 func main() {
-	todos := todo.Todos{}
+	todoStorage := storage.NewStorage[todo.Todos]("todos.json")
+
+	var todos todo.Todos
+	err := todoStorage.Load(&todos)
+	if err != nil {
+		fmt.Println("Nenhum arquivo encontrado, iniciando lista vazia ...")
+	}
+
 	todos.Add("Buy milk")
 	todos.Add("Buy bread")
 	todos.Add("Read 1 page")
-	todos.Complete(1)
 	todos.Print()
 
-
+	err = todoStorage.Save(todos)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
--- storage/storage.go
+++ storage/storage.go
@@ -8,24 +8,24 @@ import (
 type Storage[T any] struct {
 	FileName string
 }
- func newStorage[T any] (fileName string) *Storage [T] {
+
+func NewStorage[T any](fileName string) *Storage[T] {
 	return &Storage[T]{FileName: fileName}
 }
 
-func (s *Storage[T]) Save(data T) error{
+func (s *Storage[T]) Save(data T) error {
 	fileData, err := json.MarshalIndent(data, "", "")
-	if err != nil{
-		return err 
+	if err != nil {
+		return err
 	}
 	return os.WriteFile(s.FileName, fileData, 0644)
 }
 
-func (s *Storage[T]) Load(data *T) error{
+func (s *Storage[T]) Load(data *T) error {
 	fileData, err := os.ReadFile(s.FileName)
 
-	if err != nil{
+	if err != nil {
 		return err
 	}
-
-	return.json.Unmarshal(filefileData, data)
+	return json.Unmarshal(fileData, data)
 }
